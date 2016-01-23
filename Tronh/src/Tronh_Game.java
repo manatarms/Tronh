ca91cdd9970105469452e2ef3e090bdccfdc966a
Aj6 <Aj6>
2016-01-23 06:13:14 +0530

    Added initial movement to box

---
+++ Tronh/src/Tronh_Game.java
@@ -0,0 +1,118 @@
+import java.awt.Color;
+import java.awt.Graphics2D;
+import java.awt.Image;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
+
+import arcadia.Arcadia;
+import arcadia.Button;
+import arcadia.Game;
+import arcadia.Input;
+import arcadia.Sound;
+import basicGame.BasicGame;
+import intro.IntroGame;
+
+public class Tronh_Game extends Game{
+	
+    float y = 10;
+    float x = 10;
+    float velocity = 0;
+    float gravity = .5f;
+    boolean canRun = false;
+    boolean right = false;
+    boolean left = false;
+    boolean up = false;
+    boolean down = false;
+    Image banner;
+    
+    public Tronh_Game(){
+    	try{
+    	banner = ImageIO.read(Tronh_Game.class.getResource("banner.png"));
+    	}
+    	catch(IOException e){
+    		e.printStackTrace();
+    	}
+    	}
+    
+	@Override
+	public void tick(Graphics2D g, Input p1, Input p2, Sound s) {
+		
+		g.setColor(Color.GREEN);
+		
+		g.fillRect(0, 0, WIDTH, HEIGHT);
+		
+		
+		
+		if(p1.pressed(Button.R))
+		{
+			canRun = true;
+			right = true;
+			left = false;
+			up = false;
+			down = false;
+		}
+		if(p1.pressed(Button.L)){
+			canRun = true;
+			right = false;
+			left = true;
+			up = false;
+			down = false;
+		}
+		if(p1.pressed(Button.U)){
+			canRun = true;
+			right = false;
+			left = false;
+			up = true;
+			down = false;
+		}
+		if(p1.pressed(Button.D)){
+			canRun = true;
+			right = false;
+			left = false;
+			up = false;
+			down = true;
+		}
+		
+		
+		if(canRun && right){
+			velocity = 10;
+			x += velocity;
+		}
+		
+		if(canRun && left){
+			velocity = 10;
+			x -= velocity;
+		}
+		
+		if(canRun && up){
+			velocity = 10;
+			y -= velocity;
+		}
+		
+		if(canRun && down){
+			velocity = 10;
+			y += velocity;
+		}		
+		
+
+		g.setColor(Color.BLACK);
+		g.fillRoundRect((int)x,(int)y,50, 50, 20, 20);
+		
+	}
+
+	@Override
+	public void reset() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public Image banner() {
+		// Dimensions : 512 x 128
+		return banner;
+	}
+	public static void main(String[] args) {
+		Arcadia.display(new Arcadia(new Game[] {new Tronh_Game(), new IntroGame(), new BasicGame()}));
+	}
+}
