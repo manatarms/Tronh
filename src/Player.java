71fff3323ace64d4aae11b8d359ade9e8f45f2f6
ajayvasu <avasu@mtu.edu>
2016-02-25 09:11:07 +0530

    Update Player.java

--- src/Player.java
+++ src/Player.java
@@ -1,4 +1,28 @@
+import java.awt.Color;
+import java.awt.Graphics2D;
 
-public class Player {
-
+public class Move {
+      void Up(Graphics2D g){
+        g.drawImage(player_U, (int) x, (int) y, null);
+			  velocity = 10;
+			  y -= velocity;
+      }
+      
+      void Down(Graphics2D g){
+        g.drawImage(player_D, (int) x, (int) y, null);
+			  velocity = 10;
+			  y += velocity;
+      }
+      
+      void Left(Graphics2D g){
+        g.drawImage(player_L, (int) x, (int) y, null);
+			  velocity = 10;
+			  x -= velocity;
+      }
+      
+      void Right(Graphics2D g){
+        g.drawImage(player_R, (int) x, (int) y, null);
+			  velocity = 10;
+			  x += velocity;
+      }
 }
