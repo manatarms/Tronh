5ddaf08f28d7f65bf1d72521824b3e526b92486d
manatarms <fernandes.sachin@gmail.com>
2016-02-27 12:36:42 -0500

    Changed strings to dot equals

--- src/Enemy.java
+++ src/Enemy.java
@@ -58,10 +58,10 @@ final class Enemy {
 		}
 	}
 	public int getEnemyWidth(String dir){
-		if(dir == "UP"||dir == "up" ||dir == "DOWN"||dir == "down"){
+		if(dir.equals("UP")||dir.equals("up") ||dir.equals("DOWN")||dir.equals("down")){
 			return enemy_U.getWidth(null);
 		}
-		else if(dir == "LEFT"||dir == "left"||dir == "RIGHT"||dir == "right"){
+		else if(dir.equals("LEFT")||dir.equals("left")||dir.equals("RIGHT")||dir.equals("right")){
 			return enemy_L.getWidth(null);
 		}
 		else{
@@ -88,18 +88,19 @@ final class Enemy {
 	}
 
 	public void setDirection(String dir){
-		if(dir == "UP"||dir == "up"){
+		if(dir.equals("UP")||dir.equals("up")){
 			Direction = "UP";
 		}
-		else if(dir == "DOWN"||dir == "down"){
+		else if(dir.equals("DOWN")||dir.equals("down")){
 			Direction = "DOWN";
 		}
-		else if(dir == "LEFT"||dir == "left"){
+		else if(dir.equals("LEFT")||dir.equals("left")){
 			Direction = "LEFT";
 		}
-		else if(dir == "RIGHT"||dir == "right"){
+		else if(dir.equals("RIGHT")||dir.equals("right")){
 			Direction = "RIGHT";
 		}
+		
 		
 	}
 	
@@ -122,23 +123,23 @@ final class Enemy {
 		
 		
 		// DEFAULT (down)
-		if (dir == "down"||dir == "DOWN") {
+		if (dir.equals("down")||dir.equals("DOWN")) {
 			g.drawImage(enemy_D, x, y, null);
 			 
 		}
 
 		// UP
-		if (dir == "up"||dir == "UP") {
+		if (dir.equals("up")||dir.equals("UP")) {
 			g.drawImage(enemy_U, x, y, null);
 		}
 
 		// LEFT
-		if (dir == "left"||dir == "LEFT") {
+		if (dir.equals("left")||dir.equals("LEFT")) {
 			g.drawImage(enemy_L, x, y, null);
 		}
 
 		// RIGHT
-		if (dir == "right"||dir == "RIGHT") {
+		if (dir.equals("right")||dir.equals("RIGHT")) {
 			g.drawImage(enemy_R, x, y, null);
 		}
 	}
